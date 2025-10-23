c406cb5e2a55a495f09bee718ae7ddb82c3d621c
buugra-ctrl <buugra@gmail.com>
2025-10-24 00:49:26 +0300

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-sm font-normal uppercase tracking-wide ring-offset-background transition-all duration-200 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-sm font-medium uppercase tracking-wide ring-offset-background transition-all duration-200 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 tracking-[0.12em]",
   {
     variants: {
       variant: {
@@ -16,7 +16,7 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         contrastLight: "bg-[#002b54] text-white hover:bg-[#2163b7]",
-        contrastDark: "bg-white text-[#0A2B54] hover:bg-[#2163b7] hover:text-white",
+        contrastDark: "bg-white text-[#0A2B54] border border-[#002b54] hover:bg-[#e2e6e8] hover:text-[#0A2B54]",
       },
       size: {
         default: "h-10 px-4 py-2",
