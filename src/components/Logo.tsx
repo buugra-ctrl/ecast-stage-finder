033b23682a35c31e0c8f586e550bbc4ed6b261bd
buugra-ctrl <buugra@gmail.com>
2025-10-30 06:27:20 +0900

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -35,14 +35,14 @@ const Logo = ({ className = "", scaleOnLoad = true, scaleOnHover = false, scaleM
 
   return (
     <div
-      className={`flex items-center h-24 w-24 ${className}`}
+      className={`flex items-center h-28 w-28 ${className}`}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
       <img
         src={logo}
         alt="ecast"
-        className="h-24 w-24 object-contain transition-transform duration-300 ease-out will-change-transform"
+        className="h-28 w-28 object-contain transition-transform duration-300 ease-out will-change-transform"
         style={{
           transform: `scale(${scale})`,
           transformOrigin: 'center',
