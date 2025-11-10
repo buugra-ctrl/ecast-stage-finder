213e97321b8a3823a77093ef12010fa91d901c68
buugra-ctrl <buugra@gmail.com>
2025-11-10 23:44:11 +0900

    Update CTASection.tsx

--- src/components/CTASection.tsx
+++ src/components/CTASection.tsx
@@ -60,7 +60,7 @@ const CTASection = () => {
               <Button size="lg" className="bg-[#002b54] text-white hover:bg-[#00579e] active:bg-[#004884]">
                 Hemen Üye Ol
               </Button>
-              <Button variant="contrastDark" className="hidden md:inline-flex">
+              <Button variant="contrastDark" size="lg" className="hidden md:inline-flex">
                 Bİze Ulaşın
               </Button>
             </div>
